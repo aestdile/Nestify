adff69c5c481f817a1c701fd99cc071136095a4b
aestdile@gmail.com <aestdile@gmail.com>
2025-03-03 11:50:40 +0500

    Add project files.

---
+++ Nestify.Api/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nestify.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
