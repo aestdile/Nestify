f8f453315932b087637bf134ca7df2da298248a7
aestdile@gmail.com <aestdile@gmail.com>
2025-03-12 22:24:26 +0500

    BROKERS: Implement Logging

---
+++ Nestify.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,17 @@
+﻿//==================================================
+// Welcome! I'm Mukhtor C#.Net Junior Developer
+// Residental Training Software
+//==================================================
+
+
+using System;
+
+namespace Nestify.Api.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+
+    }
+}
