9764b1fb067ad7fd82e6a17f6bbb46d9ad3d0f7a
aestdile@gmail.com <aestdile@gmail.com>
2025-03-12 21:12:13 +0500

    CODE RUB: Rename Variable

--- Nestify.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Logic.Add.cs
+++ Nestify.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Logic.Add.cs
@@ -23,12 +23,12 @@ namespace Nestify.Api.Tests.Unit.Services.Foundations.Guests
             // given
             Guest randomGuest = CreateRandomGuest();
             Guest inputGuest = randomGuest;
-            Guest returningGuest =inputGuest;
-            Guest expectedGuest = returningGuest.DeepClone();
+            Guest storageGuest =inputGuest;
+            Guest expectedGuest = storageGuest.DeepClone();
 
             this.storageBrokerMock.Setup(broker =>
             broker.InsertGuestAsync(inputGuest))
-                .ReturnsAsync(returningGuest);
+                .ReturnsAsync(storageGuest);
 
             // when
             Guest actualGuest=
