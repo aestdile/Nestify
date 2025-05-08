b8b7370b6ba8db04986b1bcffe979330aecba427
aestdile@gmail.com <aestdile@gmail.com>
2025-05-08 18:50:44 +0500

    ShouldThrowDependencyValidationOnAddIfDuplicateKeyErrorOccursAndLogItAsync -> PASS

--- Nestify.Api/Services/Foundations/Guests/GuestService.Exceptions.cs
+++ Nestify.Api/Services/Foundations/Guests/GuestService.Exceptions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using Microsoft.Data.SqlClient;
 using Nestify.Api.Models.Foundations.Guests;
 using Nestify.Api.Models.Foundations.Guests.Exceptions;
@@ -30,6 +31,13 @@ namespace Nestify.Api.Services.Foundations.Guests
 
                 throw CreateAndLogCriticalDependencyException(failedGuestStorageException); 
             }
+            catch (DuplicateKeyException duplicateKeyException)
+            {
+                var alreadyExistGuestException =
+                    new AlreadyExistGuestException(duplicateKeyException);
+
+                throw CreateAndLogDependencyValidationException(alreadyExistGuestException);
+            }
         }
 
         private GuestValidationException CreateAndLogValidationException(Xeption exception)
@@ -48,6 +56,17 @@ namespace Nestify.Api.Services.Foundations.Guests
             this.loggingBroker.LogCritical(guestDependencyException);
 
             return guestDependencyException;
+        }
+
+        private GuestDependencyValidationException CreateAndLogDependencyValidationException(
+            Xeption exception)
+        {
+            var guestDependencyValidationException =
+                new GuestDependencyValidationException(exception);
+
+            this.loggingBroker.LogError(guestDependencyValidationException);
+
+            return guestDependencyValidationException;
         }
     }
 }
