87f7f47bed61f2d73ab530d737c67fd9e59583bc
aestdile@gmail.com <aestdile@gmail.com>
2025-05-08 13:15:42 +0500

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ Nestify.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Exceptions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Nestify.Api.Models.Foundations.Guests;
+using Nestify.Api.Models.Foundations.Guests.Exceptions;
+using Xunit;
+
+namespace Nestify.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        public async Task ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Guest someGuest = CreateRandomGuest();
+            SqlException sqlException = GetSqlError();
+            var failedGuestStorageException = new FailedGuestStorageException(sqlException);
+
+            var expectedGuestDependencyException =
+                new GuestDependencyException(failedGuestStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertGuestAsync(someGuest))
+                .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Guest> addGuestTask =
+                this.guestService.AddGuestAsync(someGuest);
+
+            // then
+            await Assert.ThrowsAsync<GuestDependencyException>(async () =>
+                addGuestTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertGuestAsync(someGuest),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedGuestDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Nestify.Api/Models/Foundations/Guests/Exceptions/GuestDependencyException.cs
@@ -0,0 +1,12 @@
+﻿using Xeptions;
+
+namespace Nestify.Api.Models.Foundations.Guests.Exceptions
+{
+    public class GuestDependencyException : Xeption
+    {
+        public GuestDependencyException(Xeption innerException)
+            : base(message: "Guest dependency error occured, contact support",
+                  innerException) 
+        { }
+    }
+}
